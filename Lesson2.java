5bfd41522a92b74b06784a6460ac86262507aeca
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2021-11-30 11:27:24 +0800

    first commit

---
+++ Lesson2.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Lesson2
+{
+    public static void Main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        
+        System.out.println("Enter your name: ");
+        String name = myObj.nextLine();
+        
+        int bagNumber = 0;
+        
+        while (true){
+           System.out.println("Enter your number of bags: ");
+           bagNumber = myObj.nextInt();
+           
+           if(bagNumber < 0){
+               System.out.println("Invalid input. Number of bags cannot be lower than 0");
+               System.out.println("Please try again");
+           }
+           else{
+               break;
+           }
+        }
+        
+        
+        float bagWeight = 0;
+        int extraCharge = 0;
+        if(bagNumber > 0){
+            while (true) {
+                System.out.println("Enter the total weight of the bags (in kg): ");
+                bagWeight = myObj.nextFloat();
+                
+                if(bagWeight < 0){
+                    System.out.println("Invalid input. Weight of bags cannot be less than 0 kg");
+                    System.out.println("Please try again");
+                }
+                else if(bagWeight > 100){
+                    System.out.println("Invalid input. Weight of bags cannot be more than 100 kg");
+                    System.out.println("Please try again");
+                }
+                else{
+                    break;
+                }
+            }
+            
+            
+            if(bagNumber > 2){
+                extraCharge += (20 * (bagNumber-2));
+            }
+            
+            if (bagWeight > 22.5) {
+                extraCharge += 50;
+            }
+        }
+        System.out.println("User Info: ");
+        System.out.println("Passenger Name: " + name);
+        System.out.println("Number of bags: " + bagNumber);
+        if(bagNumber > 0){
+            System.out.println("Total Baggage Weight: " + bagWeight + " kg");
+            System.out.println("Extra Charges: $" + extraCharge);
+        }
+        else{
+            System.out.println("You have a $20 drinks voucher!");
+        }
+    }
+}
---
+++ Lesson3.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Lesson3
+{
+    public static int invalidGuess = 0;
+    
+    public static void Main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        
+        Random random = new Random();
+        int max = 20;
+        int min = 1;
+        int randomInt = random.nextInt(max - min) + min;
+        
+        System.out.println("You have to guess a number between "+min+" and "+max);
+        //System.out.println(randomInt);
+        
+        int guess = min-1;
+        int count = 0;
+        while(guess != randomInt){
+            System.out.println("Enter your guess:");
+            
+            
+            guess = guessInput();
+            while(guess < min || guess > max){
+                
+                if(guess < min){
+                    invalidGuess++;
+                    System.out.println("Your guess cannot be less than "+min+". Try again:");
+                    guess = guessInput();
+                }
+                else if(guess > max){
+                    invalidGuess++;
+                    System.out.println("Your guess cannot be more than "+max+". Try again:");
+                    guess = guessInput();
+                }
+            }
+            count++;
+            
+            if(guess > randomInt){
+                if(count == 5){
+                    System.out.println("You have guessed incorrectly too many times");
+                    System.out.println("There were "+invalidGuess+" invalid tries.");
+                    break;
+                }
+                else{
+                    System.out.println("Lower");
+                }
+            }
+            else if(guess < randomInt){
+                if(count == 5){
+                    System.out.println("You have guessed incorrectly too many times");
+                    System.out.println("There were "+invalidGuess+" invalid tries.");
+                    break;
+                }
+                else{
+                    System.out.println("Higher");
+                }
+            }
+            else if(guess == randomInt){
+                System.out.println("Congratulations, you have guessed the number in "+count+" tries!");
+                System.out.println("There were "+invalidGuess+" invalid tries.");
+            }
+        }
+    }
+    
+    public static int guessInput(){
+        Scanner obj = new Scanner(System.in);
+        int guess = 0;
+        boolean isValid = false;
+        
+        while(isValid == false){
+            if(obj.hasNextInt()){
+                guess = obj.nextInt();
+                isValid = true;
+            }
+            else{
+                System.out.println("Your guess has to be an integer. Try again: ");
+                invalidGuess++;
+                obj.next();
+            }
+            
+        }
+        
+        
+        return guess;
+    }
+}
